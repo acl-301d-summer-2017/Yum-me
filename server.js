5398edfc99c25caca2771c2856e3a839c7258597
Jennifer Elton <justjenny40@hotmail.com>
2017-09-18 15:40:27 -0700

    finished the set up files

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var app = app || {};
+"use strict";
 
-(function (module) {
+const env = require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('./public'))
 
-})(app); 
+app.listen(PORT,()=>console.log(`server started: ${PORT}`));
