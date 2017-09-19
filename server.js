94a03696180a06829f0d15bc0941d99d116194fe
David Nishikawa <triptype@gmail.com>
2017-09-19 11:43:21 -0700

    began work on yelp request proxy in restaurants.js and server.js

--- server.js
+++ server.js
@@ -3,11 +3,23 @@
 const env = require('dotenv').config();
 const express = require('express');
 const app = express();
+const superRequest = require('superagent');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('./public'))
+app.use(express.static('./public'));
+
+function proxy(request, response) {
+  console.log('recieved yelp request' + request.body);
+  superRequest
+    .post(`https://api.yelp.com/v3/businesses/${request.params[0]}?term=food&location=portland`)
+    .set('Authorization', `Bearer ${process.env.AUTHORIZATION}`)
+    .end((err, resp) => response.send(resp));
+}
+
+app.get('/yelp/*', proxy)
+
 
 app.listen(PORT,()=>console.log(`server started: ${PORT}`));
