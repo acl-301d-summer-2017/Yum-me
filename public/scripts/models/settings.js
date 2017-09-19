9fedab6e77f888afbcdca25d6eaf3dd76b4ad59e
Jennifer Elton <justjenny40@hotmail.com>
2017-09-19 10:25:47 -0700

    created object literal for model settings.js

--- public/scripts/models/settings.js
+++ public/scripts/models/settings.js
@@ -1,6 +1,14 @@
 var app = app || {};
 
 (function (module) {
+    var userSettings = {
+        wantDelivery: false,
+        distance: 2000,
+        price: 3,
+        wantOpen: false,
+        location: [45.5, 122.6]
+
+    }
 
 
 })(app); 
