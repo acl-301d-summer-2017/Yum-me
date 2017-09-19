4db948f950ee817788380df65c2e7ceeb6db9443
David Nishikawa <triptype@gmail.com>
2017-09-19 15:51:37 -0700

    got all yelp data

--- server.js
+++ server.js
@@ -13,27 +13,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('./public'));
 
-function yelpProxy(request, response) {
-  response.send(request.query)
-  client.search(request.query)
-    .then(resp => response.send(resp))
-    .catch(err => console.error(err));
 
+app.get('/yelp/search', yelpSearchProxy)
+app.get('/yelp/business', yelpBusinessProxy)
+
+function yelpSearchProxy(request, response) {
+  
+  client.search(request.query)
+    .then(resp => response.send(resp.jsonBody.businesses))
+    .catch(err => console.error(err));
 }
 
-client.search({
-  categories: "Restaurants",
-  limit: "20",
-  location: "Portland",
-  open_now: "false",
-  price: "1, 2, 3",
-  radius: "2000",
-  term: "delivery"
-})
-  .then(resp => console.log(resp))
-  .catch(err => console.error(err));
-
-app.get('/yelp/*', yelpProxy)
+function yelpBusinessProxy(request, response) { // to get photos
+  client.business(request.query.id)
+    .then(resp => response.send(resp.jsonBody.photos))
+    .catch(err => console.error(err));
+}
 
 
 app.listen(PORT,()=>console.log(`server started: ${PORT}`));
