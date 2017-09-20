5df95b0ea85c5b0df870e919250b4a4a80f2d097
Jennifer Elton <justjenny40@hotmail.com>
2017-09-20 12:25:12 -0700

    added settings control object, added miles to kilometer conversion

--- public/scripts/controllers/settingControl.js
+++ public/scripts/controllers/settingControl.js
@@ -1,8 +1,14 @@
 var app = app || {};
 
 (function (module) {
+    settingsControl = {};
     $('#userInput').submit(function(event){
+        console.log(app.userSettings.wantDelivery +'1');
         app.userSettings.wantDelivery = $('#delivery :input');
-    })
+        console.log(app.userSettings.wantDelivery);
 
+    })
+module.settingsControl = settingsControl;
 })(app); 
+
+app.settingsControl.init
