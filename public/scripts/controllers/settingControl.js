d259076dede5a3d5df905badbe1f844cc16e885c
Jennifer Elton <justjenny40@hotmail.com>
2017-09-20 17:11:39 -0700

    added functionality to settings page, commited to local storage in string value, damn yelp

--- public/scripts/controllers/settingControl.js
+++ public/scripts/controllers/settingControl.js
@@ -6,14 +6,34 @@ var app = app || {};
   
     settingsControl.display = function () {
         app.settingsView.display();
+        $('#userInput').submit(function(event){
+            event.preventDefault();
+
+            var disValue = $('#distance').val();
+            var datValue = parseFloat(disValue);
+            var deliValue = $('input.deli[type=checkbox]:checked').map(function(){return this.value;}).get();
+            var deliBool = JSON.parse(deliValue);
+            var openValue = $('input.isO[type=checkbox]:checked').map(function(){return this.value;}).get();
+            var openBool = JSON.parse(openValue);
+            var priceVal=$('input.prices[type=checkbox]:checked').map(function(){return this.value;}).get().join();
+            
+            // console.log(datValue, typeof(datValue));
+            var settings = {
+                wantDelivery: deliBool,
+                distance: datValue,
+                price: priceVal,
+                wantOpen: openBool,
+                location: app.userSettings.location,
+            };
+            localStorage.setItem('settings', JSON.stringify(settings));
+            //  console.log(app.userSettings.wantDelivery +'1');
+            // app.userSettings.wantDelivery = $('#delivery:input');
+            //  console.log(app.userSettings.wantDelivery);
+            app.homeControl.init();
+        })
     }
   
-    $('#userInput').submit(function(event){
-        console.log(app.userSettings.wantDelivery +'1');
-        app.userSettings.wantDelivery = $('#delivery :input');
-        console.log(app.userSettings.wantDelivery);
 
-    })
   
     module.settingsControl = settingsControl;
 
