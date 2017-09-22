d853bdc9fe847ed17aadbb636bb39c16a42bd456
David Nishikawa <triptype@gmail.com>
2017-09-21 22:19:59 -0700

    got settings to trigger search; added a couple more options; preset form inputs to past settings

--- public/scripts/controllers/settingControl.js
+++ public/scripts/controllers/settingControl.js
@@ -3,54 +3,54 @@ var app = app || {};
 (function (module) {
   
     let settingsControl = {};
+    let $searchAgainMessage = $('#search-again-message');
   
     settingsControl.display = function () {
         app.settingsView.display();
+        $searchAgainMessage.hide();
 
         clearInterval(app.homeView.interval);
-      
-        $('#userInput').submit(function(event){
-            event.preventDefault();
+        $('#userInput').off();
+        $('#userInput').submit(settingsControl.newSettingsHandler);
+        
+        
+    }
+    
+    settingsControl.newSettingsHandler = function(event) {
+        event.preventDefault();
 
-            var disValue = $('select.distance option:selected').val();
-            console.log(disValue);
-            app.userSettings.distance = parseFloat(disValue);
+        settingsControl.getInput();
+        app.userSettings.pushSettings();
 
-            var deliValue = $('input.deli[type=checkbox]:checked').map(function(){return this.value;}).get();
+        setTimeout(() => $searchAgainMessage.fadeIn(5000), 10000);
+        app.homeControl.refreshSearch();
+        
+        
+    };
 
-            var deliBool = function(){
-                if (deliValue == "true"){
-                    return JSON.parse(deliValue);
-                } else {
-                    return JSON.parse("false");
-                }
-            };
-            app.userSettings.wantDelivery = deliBool();
-            console.log(app.userSettings.wantDelivery);
+    settingsControl.getInput = function() {
+        /* GET DISTANCE */
+        app.userSettings.distance = parseFloat($('select.distance option:selected').val());
 
-            var openValue = $('input.isO[type=checkbox]:checked').map(function(){return this.value;}).get();
-            var openBool = function(){
-                if (openValue == "true"){
-                    return JSON.parse(openValue);
-                } else {
-                    return JSON.parse("false");
-                }
-            };
-            app.userSettings.wantOpen = openBool(); 
-            console.log(app.userSettings.wantOpen);  
+        /* GET DELIVERY */
+        var deliValue = $('input.deli[type=checkbox]:checked').map(function(){return this.value;}).get();
+        app.userSettings.wantDelivery = deliValue == 'true' ? JSON.parse(deliValue) : JSON.parse('false');
 
-            app.userSettings.price = $('input.prices[type=checkbox]:checked').map(function(){return this.value;}).get().join(', ');
-            
-            app.userSettings.pushSettings();
-            page('/');
-            app.homeView.init();
-            
-            ///NEED TO REDIRECT BACK TO HOME PAGE
-            
-        })
+        /* GET WANT-OPEN */
+        var openValue = $('input.isO[type=checkbox]:checked').map(function(){return this.value;}).get();
+        app.userSettings.wantOpen = openValue == 'true' ? JSON.parse(openValue) : JSON.parse('false'); 
+
+        /* GET PRICE RANGE */
+        app.userSettings.price = $('input.prices[type=checkbox]:checked').map(function(){return this.value;}).get().join(', ');
+
+        /* GET SEARCH TERMS */
+        app.userSettings.searchTerms = $('input#terms').val().split(' ').filter(word => /^\w{1,15}$/.test(word)).join(', ');
+        console.log(app.userSettings.searchTerms);
+        
+        /* GET SEARCH COUNT */
+        app.userSettings.maxNumBiz = parseFloat($('select.search-count option:selected').val());
 
     }
-  
 
   
     module.settingsControl = settingsControl;
